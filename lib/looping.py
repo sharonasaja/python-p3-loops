c20355806cd21051faf3ce99c8199bb6f874128a
sharonasaja <sharon.asaja@student.moringaschool>
2023-09-19 01:25:56 +0300

    completed lab

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,35 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+     countdown = 10
+     while countdown > 0:
+        print(countdown)
+        countdown -= 1
+    
+     print("Happy New Year!")
+    
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    #  Create an empty list to store squared elements
+     squared_list = []  
+    #  iterate through the interger list and Square each element and append to the new list
+     for num in int_list:
+        squared_list.append(num ** 2)  
+    
+     return squared_list
+     
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    # code goes here!fizzbuzz()
+    # iterate through the numbers 1 to 101 for each number, it checks whether it's a multiple of 3 and/or 5  
+     for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+    
